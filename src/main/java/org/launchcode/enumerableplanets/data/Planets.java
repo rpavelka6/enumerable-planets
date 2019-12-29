16da977648073f2523496588d4a68aa5f5b429c8
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-12-29 17:30:27 -0600

    Exercise 14.3 steps 15-16 complete

--- src/main/java/org/launchcode/enumerableplanets/data/Planets.java
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -2,13 +2,22 @@ package org.launchcode.enumerableplanets.data;
 
 public enum Planets {
 
-    MERCURY,
-    VENUS,
-    EARTH,
-    MARS,
-    JUPITER,
-    SATURN,
-    URANUS,
-    NEPTUNE
+    MERCURY ("Mercury"),
+    VENUS ("Venus"),
+    EARTH ("Earth"),
+    MARS ("Mars"),
+    JUPITER ("Jupiter"),
+    SATURN ("Saturn"),
+    URANUS ("Uranus"),
+    NEPTUNE ("Neptune");
 
+    private final String name;
+
+    Planets(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
