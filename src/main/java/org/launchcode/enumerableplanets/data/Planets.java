089fa33e3ed270b5a4806786e3961c43f3b74a3e
Rebecca Pavelka <pavelka6@sbcglobal.net>
2020-01-04 20:12:40 -0600

    added light years to enum

--- src/main/java/org/launchcode/enumerableplanets/data/Planets.java
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -2,22 +2,29 @@ package org.launchcode.enumerableplanets.data;
 
 public enum Planets {
 
-    MERCURY ("Mercury"),
-    VENUS ("Venus"),
-    EARTH ("Earth"),
-    MARS ("Mars"),
-    JUPITER ("Jupiter"),
-    SATURN ("Saturn"),
-    URANUS ("Uranus"),
-    NEPTUNE ("Neptune");
+    MERCURY ("Mercury", 88),
+    VENUS ("Venus", 225),
+    EARTH ("Earth", 365),
+    MARS ("Mars", 687),
+    JUPITER ("Jupiter", 4333),
+    SATURN ("Saturn", 10759),
+    URANUS ("Uranus", 30687),
+    NEPTUNE ("Neptune", 60200);
 
     private final String name;
 
-    Planets(String name) {
+    private final Integer yearLength;
+
+    Planets(String name, Integer yearLength) {
         this.name = name;
+        this.yearLength = yearLength;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getYearLength() {
+        return yearLength;
     }
 }
